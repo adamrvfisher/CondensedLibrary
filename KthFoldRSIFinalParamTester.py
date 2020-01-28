f3d354b98d9da28bcbb884d37cd8dfbf03f94247
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-01-27 19:48:24 -0800

    Update KthFoldRSIFinalParamTester.py

--- KthFoldRSIFinalParamTester.py
+++ KthFoldRSIFinalParamTester.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 27 07:08:28 2020
 
-@author: AmatVictoriaCuramIII
+@author: Adam Reinhold Von Fisher - https://www.linkedin.com/in/adamrvfisher/
+
 """
 
 #Kth fold - RSI - final param tester
