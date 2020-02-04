ed080e3197d51b14788b3efb5ccc807a90e9175e
Adam Reinhold Von Fisher <adamrvfisher@gmail.com>
2020-02-03 22:31:10 -0800

    Update SupervisedLearningModelEvaluation.py

--- SupervisedLearningModelEvaluation.py
+++ SupervisedLearningModelEvaluation.py
@@ -32,6 +32,7 @@ dataIII = dataIII[-len(data):]
 dataIV = dataIV[-len(data):]
 
 #X are features, y are log returns 
+
 #Levered equities
 X = np.array(data[['8wkBreakOutRatio', '8wkBreakDownRatio', '4wkBreakOutRatio', 
 '4wkBreakDownRatio', '2wkBreakOutRatio', '2wkBreakDownRatio', 'HigherOpen', 'LowerOpen',
@@ -47,6 +48,7 @@ X = np.array(data[['8wkBreakOutRatio', '8wkBreakDownRatio', '4wkBreakOutRatio',
 '2wkRAATRtoTAATR', '8wkATRtoRange', '4wkATRtoRange', '2wkATRtoRange', '8wkRollingAverageATRtoRange',
 '4wkRollingAverageATRtoRange', '2wkRollingAverageATRtoRange', '8wkEfficiency', 
 '4wkEfficiency', '2wkEfficiency']][50:].fillna(0))
+
 #Gold
 XII = np.array(dataII[['8wkBreakOutRatio', '8wkBreakDownRatio', '4wkBreakOutRatio', 
 '4wkBreakDownRatio', '2wkBreakOutRatio', '2wkBreakDownRatio', 'HigherOpen', 'LowerOpen',
@@ -62,6 +64,7 @@ XII = np.array(dataII[['8wkBreakOutRatio', '8wkBreakDownRatio', '4wkBreakOutRati
 '2wkRAATRtoTAATR', '8wkATRtoRange', '4wkATRtoRange', '2wkATRtoRange', '8wkRollingAverageATRtoRange',
 '4wkRollingAverageATRtoRange', '2wkRollingAverageATRtoRange', '8wkEfficiency', 
 '4wkEfficiency', '2wkEfficiency']][50:].fillna(0))
+
 #30 year Bonds
 XIII = np.array(dataIII[['8wkBreakOutRatio', '8wkBreakDownRatio', '4wkBreakOutRatio', 
 '4wkBreakDownRatio', '2wkBreakOutRatio', '2wkBreakDownRatio', 'HigherOpen', 'LowerOpen',
@@ -77,6 +80,7 @@ XIII = np.array(dataIII[['8wkBreakOutRatio', '8wkBreakDownRatio', '4wkBreakOutRa
 '2wkRAATRtoTAATR', '8wkATRtoRange', '4wkATRtoRange', '2wkATRtoRange', '8wkRollingAverageATRtoRange',
 '4wkRollingAverageATRtoRange', '2wkRollingAverageATRtoRange', '8wkEfficiency', 
 '4wkEfficiency', '2wkEfficiency']][50:].fillna(0))
+
 #Volatility
 XIV = np.array(dataIV[['8wkBreakOutRatio', '8wkBreakDownRatio', '4wkBreakOutRatio', 
 '4wkBreakDownRatio', '2wkBreakOutRatio', '2wkBreakDownRatio', 'HigherOpen', 'LowerOpen',
@@ -92,12 +96,15 @@ XIV = np.array(dataIV[['8wkBreakOutRatio', '8wkBreakDownRatio', '4wkBreakOutRati
 '2wkRAATRtoTAATR', '8wkATRtoRange', '4wkATRtoRange', '2wkATRtoRange', '8wkRollingAverageATRtoRange',
 '4wkRollingAverageATRtoRange', '2wkRollingAverageATRtoRange', '8wkEfficiency', 
 '4wkEfficiency', '2wkEfficiency']][50:].fillna(0)) 
+
 #Append extra data 
 X = np.append(X, XII, 1)
 X = np.append(X, XIII, 1)
 X = np.append(X, XIV, 1)
+
 #data is trimmed due to calculation windows
 y = np.array(data['LogRet'][50:])
+
 #display info
 print("Data shape: {}".format(X.shape))
 print("--------------------------------")
@@ -105,19 +112,27 @@ print("--------------------------------")
 #STRATIFIED K CROSS VALIDATION AKA RANDOM DATA - NON SEQUENTIAL UNLIKE IN KthFoldRSI_II.py
 LinRegrScore = cross_val_score(LinearRegression(), X, y, cv = 5)
 print('Linear Regression Average -- ', str(LinRegrScore.mean()))
+
 LassoRegrScore = cross_val_score(Lasso(), X, y, cv = 5)
 print('Lasso Regression Average -- ', str(LassoRegrScore.mean()))
+
 LassoLARegrScore = cross_val_score(LassoLars(), X, y, cv = 5)
 print('Lasso Least Angle Regression Average -- ', str(LassoLARegrScore.mean()))
+
 RidgeRegrScore = cross_val_score(Ridge(), X, y, cv = 5)
 print('Ridge Regression Average -- ', str(RidgeRegrScore.mean()))
+
 SVRegrScore = cross_val_score(SVR(), X, y, cv = 5)
 print('Support Vector Regression Average -- ', str(SVRegrScore.mean()))
+
 DTreeScore = cross_val_score(DecisionTreeRegressor(), X, y, cv = 5)
 print('Decision Tree Regression Average -- ', str(DTreeScore.mean()))
+
 AdaBoostRegrScore = cross_val_score(AdaBoostRegressor(), X, y, cv = 5)
 print('Ada Boost Regression Average -- ', str(AdaBoostRegrScore.mean()))
+
 GradBoostRegrScore = cross_val_score(GradientBoostingRegressor(), X, y, cv = 5)
 print('Gradient Boost Regression Average -- ', str(GradBoostRegrScore.mean()))
+
 print("--------------------------------")
-print("Perhaps it is time for some parameter tuning.")
+print("Time for parameter tuning.")
